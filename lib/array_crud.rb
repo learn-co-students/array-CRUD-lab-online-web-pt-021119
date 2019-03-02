67330e8b6f702be8c9c141a627c70d536385806f
Taylor Mills <taylormills24@yahoo.com>
2019-03-02 22:07:44 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,7 @@ famous_actors << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
- famous_actors = ["Idris Elba","Terrance Howard","Lorenz Tate"]
- famous_actors.unshift("wow") 
+  array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,22 +22,17 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
- famous_actors = ["wow"]
- idris_elba = famous_actors.shift
+ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
- famous_actors =["am"]
- 
- famous_actors[0]
+ array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
- famous_actors = ["wow"]
- famous_actors.first
+ array.first
 end
 
 def retrieve_last_element_from_array(array)
- famous_actors = ["arrays!"]
- famous_actors.last
+ array.last
 end
