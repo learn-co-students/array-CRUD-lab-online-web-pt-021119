b381b3dfdb1ebd80fa922573165b9bcbacef6f3d
Taylor Mills <taylormills24@yahoo.com>
2019-03-02 22:12:47 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- famous_actors = ["Idris Elba", "Terrance Howard","Larenz Tate"] 
-
-famous_actors << "arrays!"
+ array << (element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -17,8 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
- famous_actors = ["arrays!"]
- actor = famous_actors.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
